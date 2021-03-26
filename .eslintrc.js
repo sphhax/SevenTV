ec6543edf97fdaedd976eda6d06bdad9005643fc
TroyDota <troybensonsa@gmail.com>
2021-03-27 01:14:57 +0200

    fix: eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,12 @@ module.exports = {
 		chrome: true,
 	},
 	parser: '@typescript-eslint/parser',
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:json/recommended',
-	],
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:json/recommended', 'prettier/prettier'],
 	plugins: ['prettier', 'json'],
 	// add your custom rules here
 	rules: {
 		indent: ['error', 'tab'],
+		'prettier/prettier': ['error', { singleQuote: true, semi: true, tabWidth: 4, useTabs: true, printWidth: 140 }],
 		'@typescript-eslint/no-namespace': 0,
 		'@typescript-eslint/no-explicit-any': 0,
 	},
